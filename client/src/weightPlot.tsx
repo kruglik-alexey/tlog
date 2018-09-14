ae2a945f522f6b28760737f8e9a268c4034ad490
Kruglik Alexey <seldoff@gmail.com>
2018-09-15 00:55:45 +0300

    Add plot range selector

--- client/src/weightPlot.tsx
+++ client/src/weightPlot.tsx
@@ -6,13 +6,13 @@ import * as Plotly from "plotly.js-basic-dist";
 import * as createPlotlyComponent from 'react-plotly.js/factory';
 import { min, max } from "lodash-es";
 import * as regression from 'regression';
-import * as moment from 'moment';
+import { Moment } from 'moment';
 import WeightPlotRangeControl, { WeightPlotRange } from './weightPlotRange';
 
 const Plot = createPlotlyComponent(Plotly);
 
 interface PlotRecord {
-    date: moment.Moment,
+    date: Moment,
     value: number
 }
 
@@ -20,6 +20,10 @@ interface WeightPlotProps {
     fetchStatus: FetchStatus,
     weightSeries: PlotRecord[],
     bfSeries: PlotRecord[]
+}
+
+interface WeightPlotState {
+    range?: WeightPlotRange
 }
 
 function adjustRange(records: PlotRecord[], adjustUpper: boolean) : number[] {
@@ -51,25 +55,41 @@ function trendline(records: PlotRecord[]): Trendline {
     }
 }
 
-class WeightPlot extends React.PureComponent<WeightPlotProps> {
-    handleRangeChange(range: WeightPlotRange) {
-        console.log(range);
+class WeightPlot extends React.Component<WeightPlotProps, WeightPlotState> {
+    constructor(props) {
+        super(props);
+        this.state = { };
+    }
+
+    handleRangeChange = (range: WeightPlotRange) => {
+        this.setState({ range });
+    }
+
+    slice(series: PlotRecord[], range: WeightPlotRange): PlotRecord[] {
+        if (!!range) {
+            return series.filter(p => p.date >= range.fromDate && p.date <= range.toDate);
+        }
+        return series;
     }
 
     render() {
         if (this.props.fetchStatus !== FetchStatus.Fetched) {
             return <span />;
         }
-        const weightRange = adjustRange(this.props.weightSeries, false);
-        const bfRange = adjustRange(this.props.bfSeries, true);
 
-        const weightTrend = trendline(this.props.weightSeries);
-        const bfTrend = trendline(this.props.bfSeries);
+        const weightSeries = this.slice(this.props.weightSeries, this.state.range);
+        const bfSeries = this.slice(this.props.bfSeries, this.state.range);
 
-        var weightPlot = <Plot data={[
+        const weightRange = adjustRange(weightSeries, false);
+        const bfRange = adjustRange(bfSeries, true);
+
+        const weightTrend = trendline(weightSeries);
+        const bfTrend = trendline(bfSeries);
+
+        const weightPlot = <Plot data={[
             {
-                x: this.props.weightSeries.map(r => r.date.toDate()),
-                y: this.props.weightSeries.map(r => r.value),
+                x: weightSeries.map(r => r.date.toDate()),
+                y: weightSeries.map(r => r.value),
                 type: 'scatter',
                 name: 'Weight',
                 hoverinfo: 'y'
@@ -86,8 +106,8 @@ class WeightPlot extends React.PureComponent<WeightPlotProps> {
                 }
             },
             {
-                x: this.props.bfSeries.map(r => r.date.toDate()),
-                y: this.props.bfSeries.map(r => r.value),
+                x: bfSeries.map(r => r.date.toDate()),
+                y: bfSeries.map(r => r.value),
                 type: 'scatter',
                 name: 'Body Fat %',
                 hoverinfo: 'y',
--- client/src/weightPlotRange.tsx
+++ client/src/weightPlotRange.tsx
@@ -3,18 +3,20 @@ import { connect } from "react-redux";
 import { FetchStatus } from "./fetchStatus";
 import { State } from "./state";
 import * as Slider from 'rc-slider';
-import { WeightRecord } from "./records";
+import { min, max } from 'lodash-es';
+import { Moment } from "moment";
 
 const Range = Slider.createSliderWithTooltip(Slider.Range);
 
 export interface WeightPlotRange {
-    from: number,
-    to: number
+    fromDate: Moment,
+    toDate: Moment
 }
 
 interface StateProps {
     fetchStatus: FetchStatus,
-    weightLog: ReadonlyArray<WeightRecord>
+    minDate: Moment,
+    maxDate: Moment
 }
 
 interface OwnProps {
@@ -24,32 +26,73 @@ interface OwnProps {
 type WeightPlotRangeProps = StateProps & OwnProps;
 
 interface WeightPlotRangeState {
-    range: WeightPlotRange
+    range?: WeightPlotRange
 }
 
+const rangeKind = 'month';
+
 class WeightPlotRangeSelector extends React.Component<WeightPlotRangeProps, WeightPlotRangeState> {
-    constructor(props: WeightPlotRangeProps) {
+    constructor(props) {
         super(props);
-        this.state = {
-            range: {
-                from: 0,
-                to: 0
-            }
+        this.state = { };
+    }
+
+    getMinDate() {
+        return this.props.minDate.clone().startOf(rangeKind);
+    }
+
+    getMaxDate() {
+        return this.props.maxDate.clone().endOf(rangeKind);
+    }
+
+    handleRangeChange = ([from, to]) => {
+        const range = {
+            fromDate: this.getMinDate().add(from, rangeKind),
+            toDate: this.getMinDate().add(to, rangeKind)
         }
+        this.setState({ range });
+        this.props.rangeChangeHandler(range);
     }
 
     render() {
         if (this.props.fetchStatus !== FetchStatus.Fetched) {
             return <span />;
         }
-        return <Range />;
+
+        const minDate = this.getMinDate();
+        const maxDate = this.getMaxDate();
+        // +1 to make last point is 01 of month next to latest record's month
+        const max = maxDate.diff(minDate, rangeKind) + 1;
+
+        let rangeFrom, rangeTo;
+        if (!!this.state.range) {
+            rangeFrom = this.state.range.fromDate.diff(minDate, rangeKind);
+            rangeTo = this.state.range.toDate.diff(minDate, rangeKind);
+        } else {
+            rangeFrom = 0;
+            rangeTo = max;
+        }
+
+        return (<Range
+            min={0}
+            max={max}
+            dots={true}
+            allowCross={false}
+            tipFormatter={v => minDate.clone().add(v, rangeKind).format('L')}
+            value={[rangeFrom, rangeTo]}
+            onChange={this.handleRangeChange}/>);
     }
 }
 
 function mapStateToProps(state: State) : StateProps {
+    const dates = state.weightLog.records.map(r => r.date);
+    const minDate = min(dates);
+    const maxDate = max(dates);
+
     return {
         fetchStatus: state.weightLog.fetched,
-        weightLog: state.weightLog.records
+        minDate,
+        maxDate
     }
 }
 
